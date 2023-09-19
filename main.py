a81ef5952c6c620db3ef7e41d2fff501b3c3b12e
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-09-19 15:51:43 +0200

    close to end

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WVM9JHZGF4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WVM9JHZGF4');
+</script>"""
+    return "Hello from Space! 🚀"
